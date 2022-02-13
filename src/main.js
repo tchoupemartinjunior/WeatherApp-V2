ff8c3b5b005546f26fb636b5333a6f747ed32783
Tchoupe <tchoupemartinjunior@gmail.com>
2022-02-13 04:43:34 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,50 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        
+        locationUrl:"",
+        city:"",
+        weather: {current:"",location:"",},
+        condition:{}
+    },
+    mounted: function () {
+        this.init();
+    },
+
+    methods: {
+
+
+        init(){
+           
+           var url ="https://api.weatherapi.com/v1/current.json?key=fa3298ecc65740a5bb981803220802&q=Yaounde&aqi=no";
+           axios.get(url)
+                .then(function (response) {
+                        app.weather.current =(response.data.current);
+                        app.weather.location= (response.data.location);
+                        app.condition=response.data.current.condition;
+                        
+                       console.log(app.weather.current);
+                        console.log(app.weather.location);
+                    }
+
+                )
+        },
+        
+        getWeather() {
+          
+            var url = "https://api.weatherapi.com/v1/current.json?key=fa3298ecc65740a5bb981803220802&q="+app.city+"&aqi=no";
+            axios.get(url)
+                .then(function (response) {
+                        app.weather.current =(response.data.current);
+                        app.weather.location= (response.data.location);
+                        app.condition=response.data.current.condition;
+                        
+                        console.log(app.weather.current);
+                        console.log(app.weather.location);
+                    }
+
+                )
+        }
+    }
+
+});
